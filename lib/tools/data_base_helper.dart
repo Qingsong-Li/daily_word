3149db019c2905ee2dce837b3fec69bf0ce8a1d5
Qingsong-Li <2336838779@qq.com>
2025-04-16 15:16:39 +0800

    Merge remote-tracking branch 'Qingsong-Li/main'

--- lib/tools/data_base_helper.dart
+++ lib/tools/data_base_helper.dart
@@ -40,7 +40,7 @@ class DatabaseHelper {
     return await openDatabase(dbPath);
   }
 
-  /// **模糊查询成语**
+  // **模糊查询成语**
   Future<List<Map<String, dynamic>>> searchChengyu(String keyword) async {
     final Database db = await database;
 
@@ -64,7 +64,7 @@ class DatabaseHelper {
     return results;
   }
 
-  /// **随机获取一条成语**
+  // **随机获取一条成语**
   Future<Map<String, dynamic>?> getRandomChengyu() async {
     Database db = await database;
     List<Map<String, dynamic>> results = await db.query(tableChengyu,
@@ -76,13 +76,7 @@ class DatabaseHelper {
     return results.isNotEmpty ? results.first : null;
   }
 
-  /// **查询数据库中的所有成语**
-  // Future<List<Map<String, dynamic>>> getAllChengyu() async {
-  //   Database db = await database;
-  //   return await db.query(tableChengyu);
-  // }
-
-  // / **根据名称查询成语**
+  // **根据名称查询成语**
   Future<Map<String, dynamic>?> getChengyuByName(String name) async {
     Database db = await database;
     List<Map<String, dynamic>> results =
@@ -90,22 +84,18 @@ class DatabaseHelper {
     return results.isNotEmpty ? results.first : null;
   }
 
-  // /// **插入一个新的成语**
-  // Future<int> insertChengyu(Map<String, dynamic> chengyu) async {
-  //   Database db = await database;
-  //   return await db.insert(tableChengyu, chengyu);
-  // }
-
-  /// **更新成语信息**
+  // **更新成语信息**
   Future<int> updateChengyu(Map<String, dynamic> chengyu) async {
     Database db = await database;
     return await db.update(tableChengyu, chengyu,
         where: "id = ?", whereArgs: [chengyu['id']]);
   }
 
-  // /// **删除一个成语**
-  // Future<int> deleteChengyu(int id) async {
-  //   Database db = await database;
-  //   return await db.delete(tableChengyu, where: "id = ?", whereArgs: [id]);
-  // }
+  // **获取所有收藏的成语
+  Future<List<Map<String, dynamic>>> getChengyuLiked() async {
+    Database db = await database;
+    List<Map<String, dynamic>> results =
+        await db.query(tableChengyu, where: "isLike = ?", whereArgs: [1]);
+    return results;
+  }
 }
